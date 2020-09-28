56c32f07047c1a0dff2d31e2b51db78cff043cb2
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2020-09-28 18:22:50 -0300

    adding a placeholder

--- classes/class-lsx-blog-customizer-posts.php
+++ classes/class-lsx-blog-customizer-posts.php
@@ -177,7 +177,7 @@ if ( ! class_exists( 'LSX_Blog_Customizer_Posts' ) ) {
 						if ( ! empty( get_the_post_thumbnail( $post->ID ) ) ) {
 							$post_thumb_id = get_post_thumbnail_id( $post->ID );
 							if ( 'lsx-placeholder' === $post_thumb_id ) {
-								$post_image = '<img class="attachment-responsive wp-post-image lsx-responsive" src="https://place-hold.it/750x350/cccccc/969696&amp;text=750x350&amp;bold&amp;fontsize=16">';
+								$post_image = '<img class="attachment-responsive wp-post-image lsx-responsive" src="/wp-content/plugins/lsx-blog-customizer/assets/images/placeholder-350x230.jpg">';
 							} else {
 								$post_image = get_the_post_thumbnail( $post->ID, $thumb_size, array(
 									'class' => $responsive,
